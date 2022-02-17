8d858eaf6776ab077369e11dc841ffa0029046b9
Alex Kong <alexkong07@gmail.com>
2022-02-17 15:32:54 -0600

    Added Welcome Screen
    Added welcome screen functionality. Clicking Enter button takes user to main genre display screen (to be built).

--- app/src/main/java/com/example/findtune/MainActivity.kt
+++ app/src/main/java/com/example/findtune/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.findtune
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * Run at startup.
+     * Sets the content view to the welcome screen.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome_screen)
+
+        // Welcome message stuff. When enter button is clicked, go to genre display screen.
+        val enterButton: Button = findViewById(R.id.enterButton)
+        enterButton.setOnClickListener { enterGenreScreen() }
+    }
+
+    /**
+     * App moves to genre display screen.
+     */
+    private fun enterGenreScreen() {
         setContentView(R.layout.activity_main)
     }
 
